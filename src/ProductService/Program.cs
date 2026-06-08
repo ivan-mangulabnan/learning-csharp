6f3a6bd8e82278a9724b8e5e45e4a5e69fe63e0d
Ivan Mangulabnan <iamangulabnan@sscgi.com>
2026-06-08 12:35:27 +0800

    add dependency

--- src/ProductService/Program.cs
+++ src/ProductService/Program.cs
@@ -1,7 +1,10 @@
+using ProductService.Services;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
 builder.Services.AddControllers();
+builder.Services.AddScoped<IProductService, ProductService.Services.ProductService>();
 
 var app = builder.Build();
 
